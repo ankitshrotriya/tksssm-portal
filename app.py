33c39cf97558d5a109cdd51508cf02c5de908b15
ankitshrotriya <53506154+ankitshrotriya@users.noreply.github.com>
2026-07-22 11:39:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,26 +52,17 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 # --- Database Setup ---
-DB_HOST=aws-1-ap-northeast-2.pooler.supabase.com
-DB_PORT=5432
-DB_NAME=postgres
-DB_USER=postgres.cncbzdueqfmgqcjmktlk
-DB_PASS=Mahakal2202
-DB_SSLMODE=require
+
 
 
 import os
 import psycopg2
 
+import os
+import psycopg2
+
 def get_db():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        sslmode=os.getenv("DB_SSLMODE", "require")
-    )
+    return psycopg2.connect(os.environ["postgresql://postgres.cncbzdueqfmgqcjmktlk:Mahakal2202@aws-1-ap-northeast-2.pooler.supabase.com:5432/postgres?sslmode=require"])
 
 
 # ✅ Function to generate sequential account numbers
